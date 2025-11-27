089218aa96726992d488c67ae793ec31f1c4ec5d
Anbu A <anbarasuannadurai7@gmail.com>
2025-11-27 11:16:06 +0530

    Day 14 : DS Problem (DoublyLinkedList) Main function

--- Day_14_Practice_Problem/DLLMain.java
+++ Day_14_Practice_Problem/DLLMain.java
@@ -4,5 +4,14 @@ public class DLLMain {
 
     public static void main(String[] args) {
 
+            DoublyLinkedList myDLL = new DoublyLinkedList(7);
+
+            myDLL.getHead();
+            myDLL.getTail();
+            myDLL.getLength();
+
+            System.out.println("\nDoubly Linked List:");
+            myDLL.printList();
+
     }
 }
