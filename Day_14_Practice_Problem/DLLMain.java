f67e399a8de24602895ca04568f68a7e42096d72
Anbu A <anbarasuannadurai7@gmail.com>
2025-11-27 11:14:27 +0530

    Day 14 : DS Problem (DoublyLinkedList) Constructor

---
+++ Day_14_Practice_Problem/DLLMain.java
@@ -0,0 +1,8 @@
+package Day_14_Practice_Problem;
+
+public class DLLMain {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Day_14_Practice_Problem/DoublyLinkedList.java
@@ -0,0 +1,53 @@
+package Day_14_Practice_Problem;
+
+public class DoublyLinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public DoublyLinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length=1;
+    }
+
+    class Node {
+        int value;
+        Node prev;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public void printList(){
+        Node temp=head;
+        while(temp.next!=null){
+            System.out.print(temp.value + " ");
+            temp=temp.next;
+        }
+    }
+
+    public void getHead() {
+        if (head == null) {
+            System.out.println("Head: null");
+        } else {
+            System.out.println("Head: " + head.value);
+        }
+    }
+
+    public void getTail() {
+        if (head == null) {
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Tail: " + tail.value);
+        }
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+}
