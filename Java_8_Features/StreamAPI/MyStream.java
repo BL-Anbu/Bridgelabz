cde0ef4c4de5948aed40b808612917cc5af99697
Anbu A <anbarasuannadurai7@gmail.com>
2025-12-12 17:13:06 +0530

    Java 8 Features : Stream API and OOPs Pillar PPT

--- Java_8_Features/StreamAPI/MyStream.java
+++ Java_8_Features/StreamAPI/MyStream.java
@@ -1,9 +1,39 @@
 package Java_8_Features.StreamAPI;
 
-import Java_8_Features.Functional_Interface.MyFunction;
-import Java_8_Features.Functional_Interface.MyPredicate;
+import Java_8_Features.Functional_Interface.*;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Collections;
+import java.util.Comparator;
+
+//All Intermediate Operations Implemented
+//
+//You now have:
+//
+//filter()
+//
+//map()
+//
+//flatMap()
+//
+//distinct()
+//
+//sorted()
+//
+//sorted(Comparator)
+//
+//limit()
+//
+//skip()
+//
+//peek()
+//
+//mapToInt()
+//
+//parallel(), sequential()
 
 public class MyStream<T> {
 
@@ -61,12 +91,127 @@ public class MyStream<T> {
         return new MyStream<>(result);
     }
 
-    public MyStream<T> limit(int n){
+    public MyStream<T> limit(int n) {
         List<T> result = new ArrayList<>();
-        for(int i = 0;i< data.size() && i<n;i++){
+        for (int i = 0; i < data.size() && i < n; i++) {
             result.add(data.get(i));
         }
         return new MyStream<>(result);
+    }
+
+    public MyStream<T> skip(int n) {
+        List<T> result = new ArrayList<>();
+        for (int i = n; i < data.size(); i++) {
+            result.add(data.get(i));
+        }
+        return new MyStream<>(result);
+    }
+
+    public MyStream<T> peek(MyConsumer<T> consumer) {
+        List<T> result = new ArrayList<>();
+
+        for (T item : data) {
+            consumer.accept(item);
+            result.add(item);
+        }
+        return new MyStream<>(result);
+    }
+
+    public MyStream<Integer> mapToInt(MyFunction<T, Integer> function) {
+        List<Integer> result = new ArrayList<>();
+        for (T item : data) {
+            result.add(function.apply(item));
+        }
+        return new MyStream<>(result);
+    }
+
+    private boolean isParallel = false;
+
+    public MyStream<T> parallel() {
+        this.isParallel = true;
+        return this;
+    }
+
+    public MyStream<T> sequential() {
+        this.isParallel = true;
+        return this;
+    }
+
+//    Terminal Operations
+//    forEach()
+//    collect()
+//    reduce()
+//    count()
+//    min(), max()
+//    anyMatch(), allMatch(), noneMatch()
+
+    public void forEach(MyConsumer<T> consumer){
+        for(T item : data){
+            consumer.accept(item);
+        }
+    }
+
+    public List<T> collect(){
+        return new ArrayList<>(data);
+    }
+
+    public long count(){
+        return data.size();
+    }
+
+    public T findFirst(){
+        return data.isEmpty()?null:data.get(0);
+    }
+
+    public boolean anyMatch(MyPredicate<T> predicate){
+        for (T item : data){
+            if(predicate.test(item)) return true;
+        }
+        return false;
+    }
+
+    public boolean allMatch(MyPredicate<T> predicate){
+        for(T item : data){
+            if(!predicate.test(item)) return false;
+        }
+        return true;
+    }
+
+    public boolean noneMatch(MyPredicate<T> predicate){
+        for(T item : data){
+            if(predicate.test(item)) return false;
+        }
+        return true;
+    }
+
+    public T reduce(T identity, MyBinaryOperator operator){
+        T result = identity;
+        for(T item : data){
+            result = (T) operator.apply(result, item);
+        }
+        return result;
+    }
+    public T min(Comparator<T> comparator) {
+        if (data.isEmpty()) return null;
+
+        T min = data.get(0);
+        for (T item : data) {
+            if (comparator.compare(item, min) < 0) {
+                min = item;
+            }
+        }
+        return min;
+    }
+    public T max(Comparator<T> comparator) {
+        if (data.isEmpty()) return null;
+
+        T max = data.get(0);
+        for (T item : data) {
+            if (comparator.compare(item, max) > 0) {
+                max = item;
+            }
+        }
+        return max;
     }
 
 }
