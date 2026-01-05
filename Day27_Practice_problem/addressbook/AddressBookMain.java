4678297b8401b48f289589cdcff4cb8961f8ee1d
BL-Anbu <anbarasu.annadurai@bridgelabz.com>
2026-01-05 16:46:51 +0530

    modified AddressBookSystem for Json csv txt

--- Day27_Practice_problem/addressbook/AddressBookMain.java
+++ Day27_Practice_problem/addressbook/AddressBookMain.java
@@ -1,35 +1,57 @@
 package Day27_Practice_problem.addressbook;
-
-import java.util.Scanner;
-
 public class AddressBookMain {
+
+    //Need to add the dependencies to run this application
+//    <dependency>
+//    <groupId>com.opencsv</groupId>
+//    <artifactId>opencsv</artifactId>
+//    <version>5.9</version>
+//    </dependency>
+//    <dependency>
+//    <groupId>com.google.code.gson</groupId>
+//    <artifactId>gson</artifactId>
+//    <version>2.10.1</version>
+//    </dependency>
+
+// Add this dependency in the settings -> project structures -> Libraries
+
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Address Book Program");
 
-        Scanner sc = new Scanner(System.in);
-        AddressBookSystem system = new AddressBookSystem();
+        AddressBook book = new AddressBook();
 
-        system.addAddressBook("Personal");
-        AddressBook book = system.getAddressBook("Personal");
-
-        ContactPerson p1 = new ContactPerson(
+        book.addContact(new ContactPerson(
                 "Anbu", "A", "Street1", "Chennai",
-                "TN", "600001", "9876543210", "anbu@mail.com");
+                "TN", "600001", "9876543210", "anbu@mail.com"));
 
-        ContactPerson p2 = new ContactPerson(
+        book.addContact(new ContactPerson(
                 "Raj", "K", "Street2", "Bangalore",
-                "KA", "560001", "9999999999", "raj@mail.com");
+                "KA", "560001", "9999999999", "raj@mail.com"));
 
-        book.addContact(p1);
-        book.addContact(p2);
-
+        System.out.println("\n--- Sort By Name ---");
         book.sortByName();
-        book.getContacts().forEach(System.out::println);
+
+        System.out.println("\n--- Sort By City ---");
+        book.sortByCity();
+
+        System.out.println("\n--- Sort By State ---");
+        book.sortByState();
+
+        System.out.println("\n--- Sort By Zip ---");
+        book.sortByZip();
 
         FileIOService fileIO = new FileIOService();
         fileIO.writeToFile(book.getContacts());
         fileIO.readFromFile();
+
+        CSVFileService csvService = new CSVFileService();
+        csvService.writeCSV(book.getContacts());
+        csvService.readCSV();
+
+        JSONFileService jsonService = new JSONFileService();
+        jsonService.writeJSON(book.getContacts());
+        jsonService.readJSON();
     }
 }
